18bf3f2681ffa5817a131a4dd7b7872f1f5dc7d7
Tobias Egelund <egelundtobias@gmail.com>
2022-04-18 19:54:03 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 from crawler import VERSION
 
-with open("requirements/base.txt") as f:
-    required = f.read().splitlines()
+# with open("requirements/base.txt") as f:
+#     required = f.read().splitlines()
 
 setup(
     name="crawler",
@@ -13,7 +13,15 @@ setup(
     A CLI program to crawl and scrape websites for scpecific types
     of files, i.e. images, videos or audio files
     """,
-    install_requires=required,
+    install_requires=[
+        "bs4>=0.0.1",
+        "requests>=2.27.1",
+        "requests-html>=0.10.0",
+        "tqdm>=4.63.0",
+        "click>=8.0",
+        "python-dotenv>=0.19.2",
+        "nest-asyncio>=1.5.4",
+    ],
     entry_points={
         "console_scripts": [
             "crawler=crawler.cli:main",
