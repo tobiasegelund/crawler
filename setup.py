7dafc3a1b4f39e3af9878cada1bc674960f12320
Tobias Egelund <egelundtobias@gmail.com>
2022-04-24 18:16:23 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,21 @@
 from setuptools import setup, find_packages
-from crawler import VERSION
-
-# with open("requirements/base.txt") as f:
-#     required = f.read().splitlines()
 
 setup(
     name="crawler",
-    version=VERSION,
+    version="0.1.1",
     author="Tobias Egelund",
     packages=find_packages(exclude=["tests*"]),
     description="""
     A micro crawl tool to scrape audio, video and image files from websites
     """,
-    classifiers = """
+    classifiers="""
         Environment :: Web Environment
         Framework :: Crawler
         Intended Audience :: Developers
         Operating System :: OS Independent
         Programming Language :: Python :: 3.8
         Programming Language :: Python :: 3.9
-        Topic ::Web Crawl :: Web Scrape :: Application Frameworks
+        Topic :: Web Crawl :: Web Scrape :: Application Frameworks
     """,
     install_requires=[
         "bs4>=0.0.1",
